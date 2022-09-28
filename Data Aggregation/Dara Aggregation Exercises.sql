f7c4297f17bba0b04ff31ab25ef74aa5189c122e
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-09-28 22:13:21 +0300

    Tasks 1 to 3

--- Data Aggregation/Dara Aggregation Exercises.sql
+++ Data Aggregation/Dara Aggregation Exercises.sql
@@ -41,6 +41,16 @@ GROUP BY `department_id`
 HAVING `third_highest_salary` IS NOT NULL
 ORDER BY `department_id`;
 
+SELECT `department_id`, (
+	SELECT DISTINCT `salary` FROM `employees` e2
+	WHERE e1.department_id = e2.department_id
+	ORDER BY `salary` DESC
+	LIMIT 1 OFFSET 2 
+) AS `third_highest_salary` FROM `employees` e1
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
+
 -- 17. Salary Challenge
  SELECT `first_name`, `last_name`, `department_id` FROM `employees` e1
  WHERE `salary` > (
