40ed07277706ac60a089830eb872a85aed6bb000
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-09-28 22:29:14 +0300

    Added Task 4

--- Table Relations/Table Relations Exercise.sql
+++ Table Relations/Table Relations Exercise.sql
@@ -105,3 +105,14 @@ CREATE TABLE teachers (
 `manager_id` INT
 );
 
+INSERT INTO teachers
+VALUES (101, "John", NULL),
+(102, "Maya", 106),
+(103, "Silvia", 106),
+(104, "Ted", 105),
+(105, "Mark", 101),
+(106, "Greta", 101);
+
+ALTER TABLE teachers
+ADD CONSTRAINT fk_manager_teacher
+FOREIGN KEY(manager_id) REFERENCES teachers(teacher_id);
