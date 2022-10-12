d2356c8b28c100ad44dcbf6c717e331469045df2
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-10-12 23:59:57 +0300

    Tasks 10 & 11

--- Exam prep/Database Basics MySQL Retake Exam - 06 August 2021.sql
+++ Exam prep/Database Basics MySQL Retake Exam - 06 August 2021.sql
@@ -141,3 +141,31 @@ WHERE g.`release_date` IS NULL AND gc.`category_id` IS NULL
 ORDER BY g.`name`;
 
 -- 10. Find all basic information for a game
+DELIMITER $$
+CREATE FUNCTION udf_game_info_by_name (game_name VARCHAR (20))
+RETURNS VARCHAR(750)
+DETERMINISTIC
+BEGIN
+DECLARE game_info VARCHAR(750);
+DECLARE team_name VARCHAR(40);
+DECLARE address_text VARCHAR(50);
+SET team_name := (SELECT t.`name` FROM `teams` AS t 
+JOIN `games` AS g ON g.`team_id` = t.`id`
+WHERE g.`name` = game_name);
+SET address_text := (SELECT a.`name` FROM `addresses` AS a
+JOIN `offices` AS o ON o.`address_id` = a.`id`
+JOIN `teams` AS t ON t.`office_id` = o.`id`
+WHERE t.`name` = team_name);
+SET game_info := CONCAT('The ', game_name, ' is developed by a ', team_name, ' in an office with an address ', address_text);
+RETURN game_info;
+END$$
+
+-- 11. Update budget of the games
+CREATE PROCEDURE udp_update_budget(min_game_rating FLOAT)
+BEGIN
+	UPDATE `games` AS g
+LEFT JOIN `games_categories` AS gc ON gc.`game_id` = g.`id`
+SET g.`budget` = g.`budget` + 100000, g.`release_date` = ADDDATE(g.`release_date`, INTERVAL 1 YEAR)
+WHERE gc.`category_id` IS NULL AND g.`rating` > 5 AND g.`release_date` IS NOT NULL;
+END$$
+DELIMITER ;
